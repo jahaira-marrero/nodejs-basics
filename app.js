af75025027fd091ff2b7e7c32f2a036bc1109ee5
Jahaira <jahaira.marrero@gmail.com>
2021-06-07 20:00:04 -0400

    Testing res.end

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const http = require('http');
 const port = 5050;
 const server = http.createServer((req, res) => {
-    res.end('
-        <html>
-            <head>
-                <title>First Node App</title>
-            </head>
-            <body>
-                <h1> Welcome to my new app!</h1>
-            </body>
-        </html>
-        ');
+    res.end('<html><head><title>First Node App</title></head><body><h1> Welcome to my new app!</h1></body></html>');
 });
 
 server.listen(port,()=> {
