7d2d56452ab0e06c3b65c0f15e59b831200f0d66
Jahaira <jahaira.marrero@gmail.com>
2021-06-29 11:06:52 -0400

    switch and cases

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 const port = 5050;
 const server = http.createServer((req, res) => {
+    switch(req.url) {
+        case '/':
     res.end('<html><head><title>First Node App</title></head><body><h1> Welcome to my new app!</h1></body></html>');
+    break;
+    case '/admin':
+        res.end('Welcome to admin page');
+    break;
+    case '/user':
+        res.end('<h1>Welcome to the user page</h1>');
+    break;
+    default:
+        res.end('not found')
+    };
 });
 
 server.listen(port,()=> {
